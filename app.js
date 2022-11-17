f1eb8c1865afc3bf6a76a1ec843a20087171e2e2
Austin Smart <asmart018@gmail.com>
2022-11-17 14:58:23 -0500

    JS_Operators Complete

--- app.js
+++ app.js
@@ -6,14 +6,64 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(a, b, add, minus, multiply, dividing);
+
 // YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+let test1 = num + str;
+let test2 = num + str;
+let test3 = num + isPresent;
+let test4 = firstName + num;
+let test5 = isPresent + str;
+let test6 = firstName + lastName;
+
+console.log(test1, test2, test3, test4, test5, test6);
 // YOUR CODE HERE
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+let test7 = val == str3;
+let test8 = val === str3;
+let test9 = !isPresent2;
+let test10 = "eleven" == str4 && val >= str3;
+let test11 = !isPresent2 || isPresent2;
+let test12 = 0 == false;
+let test13 = 0 === false;
+let test14 = 0 != false;
+let test15 = 0 !== false;
+
+console.log(
+  test7,
+  test8,
+  test9,
+  test10,
+  test11,
+  test12,
+  test13,
+  test14,
+  test15
+);
 // YOUR CODE HERE
